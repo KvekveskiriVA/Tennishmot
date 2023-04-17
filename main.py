d2cd980e0be74fc42abf800c7fc0ed5fabf41ecd
Vladislav <nerokvent7@gmail.com>
2023-04-17 17:24:34 +0300

    ses

--- main.py
+++ main.py
@@ -1,7 +1,17 @@
-import requ
-ests
-sess = requests.Session()
-sess.get('https://httpbin.org/cookies/set/sessioncookie/123456789')
-resp = sess.get('https://httpbin.org/cookies')
-print(resp.text)
-sadsad
+import requests
+
+
+# Важно: Меняем только кукисы, остальные параметры оставляет неизменными
+headers = {
+      "authority": "www.itf-tennis-point.com",
+      "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko)"
+                    " Chrome/112.0.0.0 Safari/537.36 Edg/112.0.1722.48",
+
+      "Cookie": ""
+}
+
+
+session = requests.session() # Создаём новую сессию для без блокировочного хождения по сайту
+session.headers = headers # Задаём параметры заголовков запрос с кукисами
+
+
